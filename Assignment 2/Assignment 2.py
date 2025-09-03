07dc7f9183c6d5d29eb00a75e18c6d1803fb5aa9
hg-little <hector.gonzalez001@albright.edu>
2025-09-03 08:36:05 -0400

    Assingment 2

---
+++ Assignment 2/Assignment 2.py
@@ -0,0 +1,11 @@
+message = "hello python world"
+print(message)
+
+message = "hello python crash course world "
+print(message)
+
+V = "I like chocolate"
+print(V)
+
+V = "i hate tomatoes"
+print(V)

6e9b4432ed72bde5938e23371024f04fab407a2d
hector <hector.gonzalez001@albright.edu>
2025-09-10 09:41:13 -0400

    did assignment 3

---
+++ Assignment 3/python.work.py
@@ -0,0 +1,12 @@
+names = ['hector', 'juylan', 'matthew']
+for name in names:
+    print(names)
+
+
+names = ['hector', 'juylan', 'matthew']
+for name in names:
+    print(f"Hello, {name}! hope youre having an amzing day")
+
+transportation = ['tesla','lamborghini','yamaha scooter']
+for vechile in transportation:
+    print(f"I would like to own a {vechile}")

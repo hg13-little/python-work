322d96d56e09fdb8fa587421718150ae03776a9d
hector <hector.gonzalez001@albright.edu>
2025-09-15 09:39:35 -0400

    class work

--- Assignment 4/week4.py
+++ Assignment 4/week4.py
@@ -1,6 +1,6 @@
 menu = ("french fries", "burgers", "tacos", "salad", "Sandwiches","tacos")
 
-print("original menu:")
+print("Original menu:")
 for food in menu:
     print(f"- {food}")
 try:
@@ -9,6 +9,7 @@ except TypeError as e:
     print("\nError: Cannot modify a tuple. Tuples are immutable")
     print(f"python says: {e}") 
 menu = ("french fries", "Burgers", "Tacos", "sushi", "nachos", ) 
+
 print("\Revised Menu:")
 for food in menu:
     print(f"- {food}")
